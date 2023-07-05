7c7f667b1efdbe702e6e85aee7027da33ab3b488
SamarthVP <samarth28@hotmail.com>
2023-07-05 16:32:44 -0400

    OpenAI API exception handling

--- backend/bot_endpoints.py
+++ backend/bot_endpoints.py
@@ -1,12 +1,19 @@
 from fastapi import APIRouter
 from qa_sys import agent
+from openai import error
 
 router = APIRouter(prefix="/qa")
 
 @router.post("/")
 async def answer(query: str):
-    # Need to catch and handle an OpenAI timeout due to networking issues or ratelimit
-    # As well as catching langchain.schema.output_parser.Output ParserException for LLM string output that is not a json
-    response = agent["agent"].run(agent["prompt"].format(query=query))
+    try:
+        response = agent["agent"].run(agent["prompt"].format(query=query))
+
+    except (error.RateLimitError, error.Timeout) as e:
+        # Ratelimit and invalid openai request langchain nondeterministic bug response
+        return {"response": "Sorry, I'm feeling a little overloaded, could you ask again in a few minutes?"}
+    except (error.InvalidRequestError) as e:
+        return {"response": "Sorry, could you be more specific?"}
+
     return {"response": response}
 
