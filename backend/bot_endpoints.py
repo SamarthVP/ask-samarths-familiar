0f85b06e439ad055e43bce3376105c9cb2817aee
SamarthVP <samarth28@hotmail.com>
2023-07-07 14:32:09 -0400

    Basic logging middleware-ish

--- backend/bot_endpoints.py
+++ backend/bot_endpoints.py
@@ -2,8 +2,21 @@ from fastapi import APIRouter
 from qa_sys import agent
 from openai import error
 from pydantic import BaseModel
+from fastapi.routing import APIRoute
+from typing import Callable
+from fastapi import APIRouter, Request, Response
+class LoggingRoute(APIRoute):
+    def get_route_handler(self) -> Callable:
+        original_route_handler = super().get_route_handler()
 
-router = APIRouter(prefix="/api/qa")
+        async def custom_route_handler(request: Request) -> Response:
+            response: Response = await original_route_handler(request)
+            print(f"Responded to {await request.body()} with {response.body}")
+            return response
+
+        return custom_route_handler
+
+router = APIRouter(prefix="/api/qa", route_class=LoggingRoute)
 
 class QARequest(BaseModel):
     query: str
@@ -18,7 +31,6 @@ async def answer(request: QARequest):
         return {"response": "Sorry, I'm feeling a little overloaded, could you ask again in a few minutes?"}
     except (error.InvalidRequestError) as e:
         # Honestly this is so nondeterministic that correct behaviour might be to just retry...
-        print(e)
         return {"response": "Sorry, I was distracted by a squirrel, could you be more specific or repeat the question?"}
 
     return {"response": response}
